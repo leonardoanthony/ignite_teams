6a772af859471e16a593ebe0ab01da7e4d31e138
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2024-12-03 15:08:46 -0300

    :sparkles: feat: Importando imagens

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,7 @@
-import { Text, View } from "react-native";
+import Groups from "@/src/screens/Groups";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Groups />
   );
 }
