30fdc6001c3f1cba54e8dedc5034fd9ca0ee0b95
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2024-12-04 18:07:45 -0300

    :sparkles: feat:  Fontes personalizadas

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
+import { ActivityIndicator } from "react-native";
+
 import Groups from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "@/src/theme";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
 export default function Index() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
