14bd402a0f358d33b2e07315bde86988cd6340a1
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2024-12-04 11:00:05 -0300

    :sparkles: feat: Utilizando styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,12 @@
-import { Image, Text, View } from "react-native";
-import logo from '@assets/logo.png';
+// import { Image, Text, View } from "react-native";
+// import logo from '@assets/logo.png';
+
+import { Container, Title } from "./style";
 
 export default function Groups() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image source={logo}/>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
