7685a125d13f51c0700cd69b8f45612bcdc6c60e
leonardoanthony <leonardoanthony.dev@gmail.com>
2024-12-04 21:00:45 -0300

    :lipstick: style: Estilizando Componente de Loading

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import Groups from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "@/src/theme";
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from "@/src/componentes/Loading";
 
 export default function Index() {
 
@@ -11,7 +12,7 @@ export default function Index() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
