a7eb71696938b1e2865e2d8b0dc5564a2079815d
leonardoanthony <leonardoanthony.dev@gmail.com>
2024-12-30 16:24:37 -0300

    :sparkles: feat: Criando Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/src/componentes/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@/src/componentes/Highlight";
+import { Button } from "@/src/componentes/Button";
+import { Input } from "@/src/componentes/Input";
 
 export default function NewGroup(){
     return (
@@ -13,6 +15,15 @@ export default function NewGroup(){
                 <Highlight 
                     title="Nova Turma"
                     subtitle="crie a turma para adicionar as pessoas"
+                />
+
+                <Input 
+                    placeholder="Nome da turma"
+                />
+
+                <Button 
+                    title="Criar"
+                    style={{marginTop: 20}}
                 />
 
             </Content>
